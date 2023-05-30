061b33565c335305460a9194de76da753181f4e9
saiminh <saiminh@gmail.com>
2023-05-30 21:58:46 +0200

    meta and zIndex

--- uncommon-page-nav.php
+++ uncommon-page-nav.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Uncommon Page Nav
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       On-Page Navigation between page sections
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           1.0
+ * Author:            Simon Flöter
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       uncommon-page-nav
